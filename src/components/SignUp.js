61d19099a68da1f337a9b49c4c07a9206406c3d3
litundinu <litundinu82@gmail.com>
2019-12-10 15:42:30 +0100

    react020

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,37 +1,70 @@
 import React, {Component} from 'react';
+import './SignUp.css'
 
 class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: 'test@email.test'
+            email: "mon@email.com",
+            name: "James",
+            lastname: "Bond",
+            password: "MyPassword",
+            passwordConfirmation: ""
         
         }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        // this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+        console.log(this.state)
     }
     
-    handleChange(event) {
-        this.setState({value: event.target.value});
-      };
+    handleUserInput (e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({[name]: value});
+      }
+
 
     handleSubmit = (event) => {
         event.preventDefault()
-        
-        
-
     }
-
+    
   
     render() {
-    return(<div>
-        <form onSubmit={this.handleSubmit}>
-            <label><h1>{this.state.value}</h1></label>
-            <input type="email" name="email" onChange={this.handleChange} />
+    return(
+    <div>
+            <h5>
+                <ul>
+                    <li>{this.state.email}</li>
+                    <li>{this.state.name}</li>
+                    <li>{this.state.lastname}</li>
+                    <li>{this.state.password}</li>
+                    <li>{this.state.passwordConfirmation}</li>
+                </ul>
+            </h5>
+            <form onSubmit={this.handleSubmit}>
+
+                <label><h4>Email</h4></label>
+                <input type="email" name="email" onChange={(event) => this.handleUserInput(event)} /><br/>
+                
+                <label>Name</label>
+                <input type="text" name="name" onChange={(event) => this.handleUserInput(event)} /><br/>
+                
+                <label>Last Name</label>
+                <input type="text" name="lastname" onChange={(event) => this.handleUserInput(event)} /><br/>
+
+                <label>Password</label>
+                <input type="password" name="password" onChange={(event) => this.handleUserInput(event)}/><br/>
+                
+                <label>Password confirmation</label>
+                <input type="password" name="passwordConfirmation" onChange={(event) => this.handleUserInput(event)}/><br/><br/>
+                
+                <input type="submit" value="Submit" /> 
             
             </form> 
-    </div>);
+    </div>
+    );
   }
+
 }
 
 
